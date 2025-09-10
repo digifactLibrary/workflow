2af40639e2f9e826a7f5971a24be2d95f5b37765
Nguyen Dinh Duy <130693469+duy12i1i7@users.noreply.github.com>
2025-09-11 01:32:50 +0700

    Update Vite config to allow specific host and IP

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['workflow.digifact.vn'], 
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://127.0.0.1:3001',
         changeOrigin: true,
       },
     },
